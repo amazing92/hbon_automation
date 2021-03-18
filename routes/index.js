65d4c8deffd1d6b97d444756ccda3351434e8e74
cuongpt <phancuongcdth12@gmail.com>
2021-03-18 18:32:21 +0700

    First commit

---
+++ routes/index.js
@@ -0,0 +1,54 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  const OBSWebSocket = require('obs-websocket-js');
+
+  const obs = new OBSWebSocket();
+  obs.connect({
+    address: 'localhost:4444',
+    password: '$up3rSecretP@ssw0rd'
+  })
+      .then(() => {
+        console.log(`Success! We're connected & authenticated.`);
+
+        return obs.send('GetSceneList');
+      })
+      .then(data => {
+        console.log(`${data.scenes.length} Available Scenes!`);
+
+        data.scenes.forEach(scene => {
+          if (scene.name !== data.currentScene) {
+            console.log(`Found a different scene! Switching to Scene: ${scene.name}`);
+
+            obs.send('SetCurrentScene', {
+              'scene-name': scene.name
+            });
+            // No longer supported!
+
+          } else {
+            console.log("asdasdasd");
+            obs.sendCallback('StartStreaming', (error) => {
+              console.log(error);
+            });
+          }
+        });
+      })
+      .catch(err => { // Promise convention dicates you have a catch on every chain.
+        console.log(err);
+      });
+
+  obs.on('SwitchScenes', data => {
+    console.log(`New Active Scene: ${data.sceneName}`);
+  });
+
+// You must add this handler to avoid uncaught exceptions.
+  obs.on('error', err => {
+    console.error('socket error:', err);
+  });
+  // res.send('Hello World!')
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
